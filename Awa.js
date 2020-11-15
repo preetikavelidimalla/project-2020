59600a4a109737d8f661aa6691090e42c7de17e1
Sushmi373 <67357405+Sushmi373@users.noreply.github.com>
2020-11-15 19:20:32 +0530

    Update Awa.js

--- Awa.js
+++ Awa.js
@@ -1,20 +1,33 @@
 
-    var slideIndex = 1;
-    showDivs(slideIndex);
-    
-    function plusDivs(n) {
-      showDivs(slideIndex += n);
-    }
-    
-    function showDivs(n) {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      if (n > x.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = x.length}
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-      }
-      x[slideIndex-1].style.display = "block";  
-    }
-
+document.querySelector(".prevBtn").addEventListener("click", () => {
+    changeSlides(-1);
+});
+document.querySelector(".nextBtn").addEventListener("click", () => {
+  changeSlides(1);
+});
+var slideIndex = 1;
+showSlides(slideIndex);
+function changeSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("Navdot");
+  if (n > slides.length) {
+     slideIndex = 1;
+  }
+  if (n < 1) {
+     slideIndex = slides.length;
+  }
+  Array.from(slides).forEach(item => (item.style.display = "none"));
+  Array.from(dots).forEach(
+     item => (item.className = item.className.replace(" selected", ""))
+  );
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " selected";
+}
     
